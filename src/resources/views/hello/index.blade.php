45268168996ab7cb653cb1a8899944942a9daa14
kazuki663 <kazkaz.t@icloud.com>
2022-06-25 16:31:37 +0900

    chapter4周力

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -15,40 +15,11 @@
   <form action="/hello" method="POST">
   <table>
     @csrf
-    @error('name')
-    <tr>
-      <th>ERROR</th>
-      <td>{{$message}}</td>
-    </tr>
-    @enderror
-    <tr>
-      <th>name:</th>
-      <td><input type="text" name="name" value="{{old('name')}}"></td>
-    </tr>
-    @error('mail')
-    <tr>
-      <th>ERROR</th>
-      <td>{{$message}}</td>
-    </tr>
-    @enderror
-    <tr>
-      <th>mail:</th>
-      <td><input type="text" name="mail" value="{{old('mail')}}"></td>
-    </tr>
-    @error('age')
-    <tr>
-      <th>ERROR</th>
-      <td>{{$message}}</td>
-    </tr>
-    @enderror
-    <tr>
-      <th>age:</th>
-      <td><input type="text" name="age" value="{{old('age')}}"></td>
-    </tr>
-    <tr>
-      <th></th>
-      <td><input type="submit" value="send"></td>
-    </tr>
+    @if (count($errors) > 0)
+    <tr><th>ERROR</th><td>{{$errors->first('msg')}}</td></tr>
+    @endif
+    <tr><th>Message: </th><td><input type="text" name="msg" value="{{old('msg')}}"></td></tr>
+    <tr><th></th><td><input type="submit" value="send"></td></tr>
   </table>
   </form>
 @endsection
