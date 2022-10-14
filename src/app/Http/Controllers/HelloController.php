8b24471795cef5f3660ab4d4510940eca8dd3da3
kazuki663 <kazkaz.t@icloud.com>
2022-10-14 11:50:20 +0900

    chapter5終了

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -6,16 +6,90 @@ use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\ControllerDoesNotReturnResponseException;
 use App\Http\Requests\HelloRequest;
+use Illuminate\Foundation\Console\Presets\React;
 use Validator;
 use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
+        // if(isset($request->id)){
+        //     $param = ['id' => $request->id];
+        //     $items = DB::select('select * from people where id = :id', $param);
+        // }else{
+        //     $items = DB::select('select * from people');
+        // }
+        // return view('hello.index', ['items' => $items]);
+
+        $items = DB::table('people')->orderBy('age', 'asc')
+        ->get();
+        return view('hello.index', ['items' => $items]);
+    }
+
+    public function post(Request $request) {
         $items = DB::select('select * from people');
         return view('hello.index', ['items' => $items]);
     }
+
+    public function add(Request $request) {
+        return view('hello.add');
+    }
+
+    public function create(Request $request) {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::table('people')->insert($param);
+        return redirect('/hello');
+    }
+
+    public function edit(Request $request) {
+        $item = DB::table('people')->where('id', $request->id)->first();
+        return view('hello.edit', ['form' => $item]);
+    }
+
+    public function update(Request $request) {
+        $param = [
+            'id' => $request->id,
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::table('people')->where('id', $request->id)->update($param);
+        return redirect('/hello');
+    }
+
+    public function del(Request $request) {
+        $item = DB::table('people')->where('id', $request->id)->first();
+        return view('hello.del', ['form' => $item]);
+    }
+
+    public function remove(Request $request) {
+        $param = ['id' => $request->id];
+        DB::table('people')->where('id', $request->id)->delete();
+        return redirect('/hello');
+    }
+
+    public function show(Request $request) {
+        $page = $request->page;
+        $items = DB::table('people')
+            ->offset($page * 3)
+            ->limit(3)
+            ->get();
+        return view('hello.show', ['items' => $items]);
+    }
+
 }
+
+// class HelloController extends Controller
+// {
+//     public function index(Request $request) {
+//         $items = DB::select('select * from people');
+//         return view('hello.index', ['items' => $items]);
+//     }
+// }
 
 // class HelloController extends Controller
 // {
