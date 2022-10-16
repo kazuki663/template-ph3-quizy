5fafa13bd9ee2eabedceda0359cc2ef64fd34d99
kazuki663 <kazkaz.t@icloud.com>
2022-10-16 15:32:59 +0900

    chapter6終了

---
+++ src/resources/views/person/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Person.index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <table>
+        <tr><th>Data</th><th>Board</th></tr>
+        @foreach($hasItems as $item)
+            <tr>
+                <td>{{$item->getData()}}</td>
+                <td>
+                    @if ($item->boards != null)
+                        <table width="100%">
+                            @foreach ($item->boards as $obj)
+                                <tr><td>{{$obj->getData()}}</td></tr>
+                            @endforeach
+                        </table>
+                    @endif
+                </td>
+            </tr>
+        @endforeach
+    </table>
+    <table>
+        <tr><th>Person</th></tr>
+        @foreach($noItems as $item)
+            <tr>
+                <td>{{$item->getData()}}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
+
+@section('footer')
+copyright 2020 tuyano
+@endsection
